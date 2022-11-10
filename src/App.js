0ea8b0ba8fcc33329ecdbba14a8c02daafe044ad
AkshayG-accruon <akshay@accruontechnologies.co.in>
2022-11-10 13:57:21 +0530

    add home button and update UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login, Home, Register, Error } from "./components";
 import PrivateRoutes from "./utils/PrivateRoutes";
@@ -9,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        {/* routes that need to be protected were passed inside this nested route */}
         <Route element={<PrivateRoutes />}>
           <Route path="/" element={<Home />} />
         </Route>
--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../styles/Error.module.css";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className={styles["errorContainer"]}>
+      <h1 className={styles["errorTxt"]}>404 not found🙄</h1>
+      <Link to="/" className={styles["link"]}>
+        <button className={styles["button"]}>Back To Home</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Error;
