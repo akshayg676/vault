edcba557364c5052162ffb0e1dd8ace21bc3dc27
AkshayG-accruon <akshay@accruontechnologies.co.in>
2022-11-11 11:17:29 +0530

    update admin dashboard redirect logic

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as Login } from "./Login";
 export { default as Register } from "./Resgister";
 export { default as Home } from "./Home";
-export { default as Admin } from "./Admin";
+export { default as Admin } from "./AdminDashboard";
 export { default as Error } from "./Error";
 export { default as Input } from "./Input";
