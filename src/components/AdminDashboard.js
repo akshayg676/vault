a23110cc98260b4dfdd00fa46a14e1ff26fa32bb
AkshayG-accruon <akshay@accruontechnologies.co.in>
2022-11-11 13:10:30 +0530

    complete admin dashboard

--- src/components/AdminDashboard.js
+++ src/components/AdminDashboard.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "../styles/Home.module.css";
 
 const Admin = () => {
-  return <div>Admin</div>;
+  const navigate = useNavigate();
+  let userList = JSON.parse(localStorage.getItem("userdata")).filter(
+    (user) => user.role !== "admin"
+  );
+
+  //function for user logout
+  const handlelogout = () => {
+    localStorage.setItem("AuthUser", false);
+    navigate("/login");
+  };
+  return (
+    <div className={styles["container"]}>
+      <h1>Welcome Admin</h1>
+      <button className={styles["logout"]} onClick={handlelogout}>
+        Logout
+      </button>
+      {userList.length > 0 ? (
+        <ul>
+          {userList.map((user) => (
+            <li>{user.username}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No registered user(s) found</p>
+      )}
+    </div>
+  );
 };
 
 export default Admin;
