a59f97f57f330288f8fe701cb23015ca71d20921
AkshayG-accruon <akshay@accruontechnologies.co.in>
2022-11-11 10:42:06 +0530

    complete login check and redirect on success

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import styles from "../styles/Register.module.css";
 import { Input } from "./index";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 
 const schema = yup
   .object({
-    email: yup
-      .string()
-      .required("Email is a required field")
-      .matches(
-        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-        "Please enter a valid email address"
-      ),
+    username: yup.string().required("Username is a required field"),
     password: yup.string().required("Passowrd is a required field"),
   })
   .required();
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  let userData = JSON.parse(localStorage.getItem("userdata"));
   const {
     register,
     handleSubmit,
@@ -27,19 +26,39 @@ const Login = () => {
   } = useForm({
     resolver: yupResolver(schema),
   });
-  const formSubmit = (data) => console.log(data);
+  const formSubmit = (data) => {
+    setLoading(true);
+    // getting the user from the localStorage by checking the submitted username
+    const currentUser = userData.find(
+      (user) => user.username === data.username
+    );
+
+    if (currentUser) {
+      //checking if the enterd password matches the current user
+      if (currentUser.password === data.password) {
+        toast.success("Sign in Successful");
+        navigate("/");
+      } else {
+        toast.error("Incorrect Password");
+      }
+    } else {
+      toast.error("Incorrect Username");
+    }
+    setLoading(false);
+  };
 
   return (
     <div className={styles["signup"]}>
+      <Toaster />
       <h1 className={styles["title"]}>Login Form</h1>
       <form onSubmit={handleSubmit(formSubmit)}>
         <Input
-          id="email"
-          placeholder="Enter your email"
-          type="email"
-          label="Email"
-          register={{ ...register("email") }}
-          errorMessage={errors.email?.message}
+          id="username"
+          placeholder="Enter your name"
+          type="text"
+          label="Username"
+          register={{ ...register("username") }}
+          errorMessage={errors.username?.message}
         />
         <Input
           id="password"
@@ -49,7 +68,10 @@ const Login = () => {
           register={{ ...register("password") }}
           errorMessage={errors.password?.message}
         />
-        <button className={styles["button"]}>Sign In</button>
+        <button disabled={loading} className={styles["button"]}>
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+
         <p className={styles["toggle"]}>
           Don't have an account?
           <Link to="/register" className={styles["link"]}>
